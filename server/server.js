f7e6dd05b1dd9a09d8095c25a4e627ff8cbe0c79
Peris Rana <ranaperis33@gmail.com>
2024-01-02 22:48:18 +0545

    server: add mongodb connection setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
 const port = 5900;
 const app = express();
-app.get('/', (req, res) => {
-   res.send('apple');
+
+dotenv.config();
+connectDB();
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("car-rental server");
 });
+
 app.listen(port, () => {
-   console.log('Server is running ...');
+  console.log("Server is running ...");
 });
