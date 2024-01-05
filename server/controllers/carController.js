32e4426f7119517fcd66b10db432d959a93a9b4a
Peris Rana <ranaperis33@gmail.com>
2024-01-05 10:00:37 +0545

    server: add route and controller for updating a car

--- server/controllers/carController.js
+++ server/controllers/carController.js
@@ -18,9 +18,25 @@ export const addCarController = async (req, res) => {
 //get-car
 export const getCarController = async (req, res) => {
   try {
-    const cars = await carModel.find();
-    res.status(200).send({ cars });
+    const car = await carModel.find();
+    res.status(200).send({ car });
   } catch (error) {
-    res.status(404).json({ message: "Error in finding the car" });
+    res.status(500).json({ message: "Error in finding the car" });
+  }
+};
+
+//update-car
+export const updateCarController = async (req, res) => {
+  try {
+    const { _id, model, details, rentalPrice } = req.body;
+    const car = await carModel.findOneAndUpdate({
+      _id,
+      model,
+      details,
+      rentalPrice,
+    });
+    res.status(200).send({ car });
+  } catch (error) {
+    res.status(500).json({ message: "Error in updating the car" });
   }
 };
