0a0fa7e0210cb43b50c781fe75ecf6027fa91525
Peris Rana <ranaperis33@gmail.com>
2024-01-05 10:00:37 +0545

    add route and controller for updating a car

--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -2,8 +2,10 @@ import express from "express";
 import {
   addCarController,
   getCarController,
+  updateCarController,
 } from "../controllers/carController.js";
 const router = express.Router();
 router.post("/add-car", addCarController);
 router.get("/get-car", getCarController);
+router.put("/update-car", updateCarController);
 export default router;
